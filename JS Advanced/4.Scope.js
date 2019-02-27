3e7eb7fab84b0c669d86046559943e6ded668b2a
Yuriy Litvinov <litvinov.yuriy1996@gmail.com>
2019-02-27 23:53:22 +0400

    Fixed 4.Scope.js

--- JS Advanced/4.Scope.js
+++ JS Advanced/4.Scope.js
@@ -17,3 +17,44 @@ const prev = counter(-1);// prev
 console.log(prev());
 console.log(prev());
 console.log(prev());
+//Доп задание
+'use strict';
+function counter(index) {
+    let current = 0;
+    let numberCall = 0;
+    let history = [];
+    return () => {
+    		numberCall++;
+        current = current + index;
+        if( history.length  == 10) {
+          history.shift();
+          history.push(current);
+        } else history.push(current);
+        return {
+        	current: current,
+          numberCurrentCall: numberCall,
+        	historyCounter: history,
+        };
+    }
+}
+const next = counter(1); // next
+function startNextCounter(delayCounter, stopCounter) {
+	const intervalId = setInterval(() => {
+  	console.log(next());
+	}, delayCounter)
+  setTimeout(() => {
+  	clearInterval(intervalId);
+  },stopCounter);
+}
+startNextCounter(100, 500);
+
+const prev = counter(-1); // next
+function startPrevCounter(delayCounter, stopCounter) {
+	const intervalId = setInterval(() => {
+  	console.log(prev());
+	}, delayCounter)
+  setTimeout(() => {
+  	clearInterval(intervalId);
+  },stopCounter);
+}
+startPrevCounter(200, 1000);
